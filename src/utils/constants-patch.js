46df83e93d44d4c0c5962ee9fd825131f2cd2f0e
Clearflo <clearfloai@gmail.com>
2025-07-26 00:39:19 -0600

    Remove 'House Blend -' prefix from house blend sheesha menu items

---
+++ src/utils/constants-patch.js
@@ -0,0 +1,15 @@
+// This is a patch file showing the changes to House Blend sheesha names
+// Remove 'House Blend - ' prefix from the following items:
+
+// s1: 'House Blend - Orange Mint' -> 'Orange Mint'
+// s2: 'House Blend - Double Apple' -> 'Double Apple' 
+// s3: 'House Blend - White Apple' -> 'White Apple'
+// s4: 'House Blend - White Grape' -> 'White Grape'
+// s5: 'House Blend - Mixed Fruit' -> 'Mixed Fruit'
+// s6: 'House Blend - Peach' -> 'Peach'
+// s7: 'House Blend - Guava' -> 'Guava'
+// s8: 'House Blend - Vanilla' -> 'Vanilla'
+// s9: 'House Blend - Pineapple' -> 'Pineapple'
+// s10: 'House Blend - Kiwi' -> 'Kiwi'
+
+// Apply these changes to src/utils/constants.ts
